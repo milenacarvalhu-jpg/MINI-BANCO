0a98effb376d33673d0258e8bfdf607782ae086e
milenacarvalhu-jpg <milenacarvalhu@gmail.com>
2026-03-16 19:45:14 -0300

    Feat: Cria estrutura inicial da Classe MiniBanco

--- MiniBanco/MiniBanco.java
+++ MiniBanco/MiniBanco.java
@@ -1,7 +1,18 @@
 package MiniBanco;
 import java.util.Scanner;
+
 public class MiniBanco {
-     
+
+static void exibirMenu(){
+System.out.println("\n=== MiniBanco ===");
+System.out.println("1 - Depositar");
+System.out.println("2 - Sacar");
+System.out.println("3 - Consultar Saldo");
+System.out.println("4 - Ver Extrato");
+System.out.println("0 - Sair");
+System.out.println("Escolha ");
+
+}
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
@@ -14,7 +25,11 @@ public class MiniBanco {
         String nome = scanner.next();
         System.out.printf(" Olá, %s! Saldo inicial: R$ %.2f\n", nome, saldo);
 
+        exibirMenu();
+
         scanner.close();
+
+
 
 
 
